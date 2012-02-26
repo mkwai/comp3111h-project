92518adad0b5b70b72a8859ef1d0e9b2b30a18a6
chanharry11@gmail.com <chanharry11@gmail.com@dbd0592c-cb16-c347-c70a-97c1e5d1de72>
2012-02-26 15:53:44 +0000

    add event interface draft version

---
+++ trunk/AndroidCalendar2/src/com/calendar/AddEvent.java
@@ -0,0 +1,226 @@
+package com.calendar;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import com.test2.R;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+public class AddEvent extends Activity {
+	private Button cancelButton;
+	private Button saveButton;
+	private Button startingDateButton;
+	private Button endingDateButton;
+	private Button startingTimeButton;
+	private Button endingTimeButton;
+
+	// for date set dialog
+	public static final int STARTING_DATE_DIALOG = 0;
+	public static final int STARTING_TIME_DIALOG = 1;
+	public static final int ENDING_DATE_DIALOG = 2;
+	public static final int ENDING_TIME_DIALOG = 3;
+
+
+	// storing the event starting/ending calendar(date+time)
+	private Calendar startingCalendar = Calendar.getInstance();
+	private Calendar endingCalendar = Calendar.getInstance();
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.addevent);
+
+		startingDateButton = (Button) findViewById(R.id.addevent_starting_date_button);
+		startingTimeButton = (Button) findViewById(R.id.addevent_starting_time_button);
+		endingDateButton = (Button) findViewById(R.id.addevent_ending_date_button);
+		endingTimeButton = (Button) findViewById(R.id.addevent_ending_time_button);
+
+		// setting up default starting/ending date/time
+		Calendar calendar = Calendar.getInstance();
+	
+		startingDateButton.setText(DateFormat.format("MMM", calendar) + " "
+				+ DateFormat.format("dd", calendar) + " , "
+				+ DateFormat.format("yyyy", calendar));
+		startingTimeButton.setText(DateFormat.format("kk", calendar) + ":"
+				+ DateFormat.format("mm", calendar));
+		endingDateButton.setText(DateFormat.format("MMM", calendar) + " "
+				+ DateFormat.format("dd", calendar) + " , "
+				+ DateFormat.format("yyyy", calendar));
+		endingTimeButton.setText(DateFormat.format("kk", calendar) + ":"
+				+ DateFormat.format("mm", calendar));
+
+		//setting up onClickListener for each button
+		startingDateButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				showDialog(STARTING_DATE_DIALOG);
+			}
+		});
+		startingTimeButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				showDialog(STARTING_TIME_DIALOG);
+			}
+		});
+
+		endingDateButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				showDialog(ENDING_DATE_DIALOG);
+			}
+		});
+
+		endingTimeButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				showDialog(ENDING_TIME_DIALOG);
+			}
+		});
+	}
+	// call when create dialog
+	protected Dialog onCreateDialog(int id) {
+
+		// listener for setting date in starting date dialog
+		OnDateSetListener startingDateSetListener = new OnDateSetListener() {
+
+			// use when "set" press
+			public void onDateSet(DatePicker view, int year, int monthOfYear,
+					int dayOfMonth) {
+				// store the date set by the dialog to startingCalendar variable
+				startingCalendar.set(year, monthOfYear, dayOfMonth);
+				// renew endingCalendar (ending > starting)
+				if (startingCalendar.after(endingCalendar)) {
+					endingCalendar.set(year, monthOfYear, dayOfMonth);
+					endingDateButton.setText(DateFormat.format("MMM",
+							startingCalendar)
+							+ " "
+							+ DateFormat.format("dd", startingCalendar)
+							+ " , "
+							+ DateFormat.format("yyyy", startingCalendar));
+				}
+				startingDateButton.setText(DateFormat.format("MMM",
+						startingCalendar)
+						+ " "
+						+ DateFormat.format("dd", startingCalendar)
+						+ " , "
+						+ DateFormat.format("yyyy", startingCalendar));
+
+			}
+		};
+		
+		// listener for setting date in ending date dialog
+		OnDateSetListener endingDateSetListener = new OnDateSetListener() {
+
+			// use when "set" press
+			public void onDateSet(DatePicker view, int year, int monthOfYear,
+					int dayOfMonth) {
+				//compareCalendar is temp, if valid input then compare -> ending
+				Calendar compareCalendar = Calendar.getInstance();
+				compareCalendar.set(year, monthOfYear, dayOfMonth);
+				compareCalendar.set(Calendar.HOUR_OF_DAY,
+						endingCalendar.get(Calendar.HOUR_OF_DAY));
+				compareCalendar.set(Calendar.MINUTE,
+						endingCalendar.get(Calendar.MINUTE));
+
+				// store the date set by the dialog to endingCalendar variable
+				// renew endingCalendar (ending > starting)
+				if (compareCalendar.after(startingCalendar)) {
+
+					endingCalendar.set(year, monthOfYear, dayOfMonth);
+					endingDateButton.setText(DateFormat.format("MMM",
+							startingCalendar)
+							+ " "
+							+ DateFormat.format("dd", startingCalendar)
+							+ " , "
+							+ DateFormat.format("yyyy", startingCalendar));
+				}
+
+			}
+		};
+		
+		// listener for setting date in starting time dialog
+		OnTimeSetListener startingTimeSetListener = new OnTimeSetListener() {
+
+			public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+				startingCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+				startingCalendar.set(Calendar.MINUTE, minute);
+
+				if (startingCalendar.after(endingCalendar)) {
+					endingCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+					endingCalendar.set(Calendar.MINUTE, minute);
+					endingTimeButton.setText(DateFormat.format("kk",
+							endingCalendar)
+							+ ":"
+							+ DateFormat.format("mm", endingCalendar));
+				}
+
+				startingTimeButton.setText(DateFormat.format("kk",
+						startingCalendar)
+						+ ":"
+						+ DateFormat.format("mm", startingCalendar));
+			}
+		};
+
+		// listener for setting date in ending time dialog
+		OnTimeSetListener endingTimeSetListener = new OnTimeSetListener() {
+
+			public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+				Calendar compareCalendar = Calendar.getInstance();
+
+				// use to get dd/mm/yy
+				compareCalendar.setTimeInMillis(endingCalendar
+						.getTimeInMillis());
+				compareCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+				compareCalendar.set(Calendar.MINUTE, minute);
+
+				if (compareCalendar.after(startingCalendar)) {
+					endingCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+					endingCalendar.set(Calendar.MINUTE, minute);
+					endingTimeButton.setText(DateFormat.format("kk",
+							endingCalendar)
+							+ ":"
+							+ DateFormat.format("mm", endingCalendar));
+				}
+
+			}
+		};
+
+		
+		//select which dialog to show by verifying which button was press
+		switch (id) {
+		case STARTING_DATE_DIALOG:
+			return new DatePickerDialog(this, startingDateSetListener,
+					startingCalendar.get(Calendar.YEAR), startingCalendar.get(Calendar.MONTH),
+					startingCalendar.get(Calendar.DAY_OF_MONTH));
+		case ENDING_DATE_DIALOG:
+			return new DatePickerDialog(this, endingDateSetListener,
+					endingCalendar.get(Calendar.YEAR), endingCalendar.get(Calendar.MONTH),
+					endingCalendar.get(Calendar.DAY_OF_MONTH));
+		case STARTING_TIME_DIALOG:
+			return new TimePickerDialog(this, startingTimeSetListener,
+					startingCalendar.get(Calendar.HOUR_OF_DAY),
+					startingCalendar.get(Calendar.MINUTE), true);
+		case ENDING_TIME_DIALOG:
+			return new TimePickerDialog(this, endingTimeSetListener,
+					endingCalendar.get(Calendar.HOUR_OF_DAY),
+					endingCalendar.get(Calendar.MINUTE), true);
+		}
+		return null;
+
+	}
+}
