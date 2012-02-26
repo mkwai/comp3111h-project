59385ca9aa874b1f35e9241bc453df0531cc3237
chanharry11 <chanharry11@gmail.com>
2012-02-26 15:53:44 +0000

    add event interface draft version

---
+++ AndroidCalendar2/src/com/calendar/AndroidCalendar2Activity.java
@@ -0,0 +1,56 @@
+package com.calendar;
+
+import com.test2.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CalendarView;
+
+public class AndroidCalendar2Activity extends Activity {
+	/** Called when the activity is first created. */
+	CalendarView calendarV;
+	Button bGoToToday, bAddEvent;
+
+	// testing123
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		// button GoToToday
+		bGoToToday = (Button) findViewById(R.id.bGoToToday);
+		bGoToToday.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				calendarV.setDate(System.currentTimeMillis());
+			}
+
+		});
+		
+		// button AddEvent
+		bAddEvent = (Button) findViewById(R.id.bAddEvent);
+		bAddEvent.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				//setContentView(R.layout.addevent);
+				startActivity(new Intent("com.calendar.ADDEVENT"));
+			}
+
+		});
+
+		// calendar view
+		calendarV = (CalendarView) findViewById(R.id.calendarV);
+		calendarV.setEnabled(true);
+		calendarV.setShowWeekNumber(false);
+
+	}
+
+}
