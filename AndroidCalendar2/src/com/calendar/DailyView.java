d81ab348c8d11fbece57cb34b53a8f8644f615fe
chanharry11 <chanharry11@gmail.com>
2012-03-10 11:10:55 +0000

    @dailyview: able to new textview as object, see if database can connect or not @addevent: fix the bug, default setting of time changed to 5 minutes interval @monthly: still thinking how to prevent enter dailyview accidently......

--- AndroidCalendar2/src/com/calendar/DailyView.java
+++ AndroidCalendar2/src/com/calendar/DailyView.java
@@ -4,6 +4,7 @@ import com.test2.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TextView;
 
@@ -11,9 +12,10 @@ public class DailyView extends Activity {
 
 	public static int dailyYear, dailyMonth, dailyDayOfMonth;
 	TextView dailyview_today;
-	TextView testing;
-	
-	
+	TextView event1, event2, event3;
+	TextView twelve_am;
+	RelativeLayout relativeLayout;
+
 	protected static void setDailyYear(int year) {
 		dailyYear = year;
 	}
@@ -29,13 +31,35 @@ public class DailyView extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.dailyview);
+		relativeLayout = (RelativeLayout) findViewById(R.id.daily_relativelayout);
 		dailyview_today = (TextView) findViewById(R.id.dailyview_today);
 		dailyview_today.setText("Today is " + dailyDayOfMonth + " / "
 				+ dailyMonth + " / " + dailyYear);
 
-		testing = new TextView(this);
-		//testing.set
+		twelve_am = (TextView) findViewById(R.id.twelve_am);
+
+		event1 = new TextView(this);
+		event2 = new TextView(this);
+		event3 = new TextView(this);
+
+		relativeLayout.addView(event1);
+		relativeLayout.addView(event2);
+		relativeLayout.addView(event3);
+
+		// get event content + location
+		event1.setText("event1");
+		event2.setText("event2");
+		event3.setText("event3");
+
+		event1.setX(100);
+		event1.setY(twelve_am.getY());
+
+		event2.setX(100);
+		event2.setY(twelve_am.getY()/* + event time changed to minutes */);
 		
+		event3.setX(100);
+		event3.setY(twelve_am.getY()/* + event time changed to minutes */);
+
 	}
 
 }
--- AndroidCalendar2/src/com/calendar/MonthlyView.java
+++ AndroidCalendar2/src/com/calendar/MonthlyView.java
@@ -5,11 +5,14 @@ import com.test2.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnDragListener;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.CalendarView.OnDateChangeListener;
@@ -18,22 +21,9 @@ public class MonthlyView extends Activity {
 
 	CalendarView calendarV;
 	Button bGoToToday, bAddEvent, bSynchronous;
-
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-
-		// button GoToToday
-		bGoToToday = (Button) findViewById(R.id.bGoToToday);
-		bGoToToday.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				calendarV.setDate(System.currentTimeMillis());
-			}
-
-		});
 
 		// button AddEvent
 		bAddEvent = (Button) findViewById(R.id.bAddEvent);
@@ -49,18 +39,17 @@ public class MonthlyView extends Activity {
 		});
 
 		// button synchronous
-		bSynchronous = (Button)findViewById(R.id.bSynchronous);
-		bSynchronous.setOnClickListener(new OnClickListener(){
+		bSynchronous = (Button) findViewById(R.id.bSynchronous);
+		bSynchronous.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
 				startActivity(new Intent("com.calendar.SYNCHRONOUS"));
-
 			}
-			
 		});
-		
+
+
 		// calendar view
 		calendarV = (CalendarView) findViewById(R.id.calendarV);
 		calendarV.setEnabled(true);
@@ -70,16 +59,16 @@ public class MonthlyView extends Activity {
 			@Override
 			public void onSelectedDayChange(CalendarView arg0, int year,
 					int month, int dayOfMonth) {
+				Log.i("abc", Long.toString(arg0.getDate()));	
 				startActivity(new Intent("com.calendar.DAILYVIEW"));
 				DailyView.setDailyYear(year);
 				DailyView.setDailyMonth(month);
 				DailyView.setDailyDayOfMonth(dayOfMonth);
 			}
-
 		});
 
 	}
- 
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
@@ -87,7 +76,7 @@ public class MonthlyView extends Activity {
 		menuInflater.inflate(R.menu.main_menu, menu);
 		return true;
 	}
- 
+
 	public boolean onOptionItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case R.id.menu_addEvent:
@@ -95,7 +84,7 @@ public class MonthlyView extends Activity {
 			return true;
 		case R.id.menu_dailyView:
 			startActivity(new Intent("com.calendar.DAILYVIEW"));
-					return true;
+			return true;
 		}
 		return false;
 
