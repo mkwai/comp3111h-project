b63593741985e5edb5bbb01f6182780d997bdce9
klhoab <klhoab@gmail.com>
2012-03-19 19:50:38 +0000

    Updated the function for getting events from a period of days.

--- AndroidCalendar2/src/com/googlesync/GoogleSync.java
+++ AndroidCalendar2/src/com/googlesync/GoogleSync.java
@@ -1,4 +1,3 @@
-
 package com.googlesync;
 
 import com.google.gdata.client.calendar.CalendarQuery;
@@ -11,11 +10,18 @@ import com.google.gdata.data.batch.BatchUtils;
 import com.google.gdata.data.calendar.CalendarEventEntry;
 import com.google.gdata.data.calendar.CalendarEventFeed;
 import com.google.gdata.data.extensions.When;
+import com.google.gdata.util.AuthenticationException;
+import com.google.gdata.util.ServiceException;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
-
 
 public class GoogleSync {
 	public class Event {
@@ -29,54 +35,76 @@ public class GoogleSync {
 
 	private static final String METAFEED_URL_BASE = "https://www.google.com/calendar/feeds/";
 	private static final String EVENT_FEED_URL_SUFFIX = "/private/full";
+	private CalendarService myService;
 	private static URL metafeedUrl = null;
 	private static URL eventFeedUrl = null;
-
 	private String userName;
 	private String userPassword;
 
-	CalendarService myService;
-	DateTime start;
-	DateTime end;
-
 	public GoogleSync() {
-
 	}
 
 	public GoogleSync(String userName, String userPassword) {
 		this.userName = userName;
 		this.userPassword = userPassword;
+	}
+
+	/*
+	 * METHOD 0 - Start Connection with userName, userPassword, return false if
+	 * the connection fails.
+	 */
+	public boolean GoogleLogin() {
+		myService = new CalendarService("applicationName");
 		try {
-			myService = new CalendarService("exampleCo-exampleApp-1");
+			myService.setUserCredentials(userName, userPassword);
 			metafeedUrl = new URL(METAFEED_URL_BASE + userName);
 			eventFeedUrl = new URL(METAFEED_URL_BASE + userName
 					+ EVENT_FEED_URL_SUFFIX);
-			myService.setUserCredentials(userName, userPassword);
-
-		} catch (Exception e) {
+		} catch (AuthenticationException e) {
+			// TODO Auto-generated catch block
+			System.out.println("User password is not matched");
+			return false;
+			// e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// Bad URL
+			System.err.println("Uh oh - you've got an invalid URL.");
+			return false;
+			// e.printStackTrace();
 		}
+		return true; // no error
 	}
 
-	// METHOD 1 - PRINT ALL
+	/*
+	 * METHOD 1 - Get all event from Google Calendar, get the details of each
+	 * event using the function: getEventEntry(entry).
+	 */
 	public void getAllEvents() {
+		CalendarEventFeed resultFeed;
 		try {
-			CalendarEventFeed resultFeed;
 			resultFeed = myService.getFeed(eventFeedUrl,
 					CalendarEventFeed.class);
+
 			for (int i = 0; i < resultFeed.getEntries().size(); i++) {
 				CalendarEventEntry entry = resultFeed.getEntries().get(i);
 				getEventEntry(entry); // get each event entry
 			}
-
-		} catch (Exception e) {
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("IOEX");
+			e.printStackTrace();
+		} catch (ServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
-	// METHOD 2 - PRINT WITHIN A RANGE OF TIME
+	/*
+	 * METHOD 2 - PRINT WITHIN A RANGE OF TIME
+	 */
 	public void getRangeEvents(DateTime startTime, DateTime endTime) {
 		try {
 			CalendarQuery myQuery = new CalendarQuery(eventFeedUrl);
-			
+
 			myQuery.setMinimumStartTime(startTime);
 			myQuery.setMaximumStartTime(endTime);
 
@@ -92,13 +120,59 @@ public class GoogleSync {
 		}
 	}
 
-	// Get info, to be stored in the database
+	/*
+	 * helper function for 2b
+	 */
+	public String addDays(String dt, int days) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Calendar c = Calendar.getInstance();
+		try {
+			c.setTime(sdf.parse(dt));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		c.add(Calendar.DATE, days); // number of days to add
+		dt = sdf.format(c.getTime()); // dt is now the new date
+		return dt;
+	}
+
+	/*
+	 * METHOD 2b - Overloaded. PRINT WITHIN A RANGE OF TIME
+	 */
+	public void getRangeEvents(String startDate, int passDay, int futureDay) {
+
+		CalendarQuery myQuery = new CalendarQuery(eventFeedUrl);
+		String t1 = addDays(startDate, -passDay);
+		String t2 = addDays(startDate, futureDay);
+
+		myQuery.setMinimumStartTime(DateTime.parseDate(t1));
+		myQuery.setMaximumStartTime(DateTime.parseDate(t2));
+
+		CalendarEventFeed resultFeed;
+		try {
+			resultFeed = myService.query(myQuery, CalendarEventFeed.class);
+
+			for (int i = 0; i < resultFeed.getEntries().size(); i++) {
+				CalendarEventEntry entry = resultFeed.getEntries().get(i);
+				getEventEntry(entry); // get each event entry
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/*
+	 * Called from method 1, 2 : Get info, to be stored in the database
+	 */
 	public void getEventEntry(CalendarEventEntry entry) {
 		assert entry != null;
-		DateFormat dfGoogle = new SimpleDateFormat("yyyy-MM-dd'T00:00:00'");
-
+		
 		Event store = new Event();
-
 		store.title = entry.getTitle().getPlainText(); // get title
 		store.description = entry.getPlainTextContent(); // get description
 		store.location = entry.getLocations().get(0).getValueString(); // get
@@ -115,101 +189,89 @@ public class GoogleSync {
 			}
 		}
 
-		// Display in console - to be removed later
-		System.out.println(store.title);
-		System.out.println(store.description);
-		System.out.println(store.location);
-		System.out.println(store.starttime);
-		System.out.println(store.endtime);
-		System.out.println();
+		/*
+		 * Display in console - to be removed later
+		 */
+		   
+		 	System.out.println(store.title);
+			System.out.println(store.description);
+			System.out.println(store.location);
+			System.out.println(store.starttime);
+			System.out.println(store.endtime);
+			System.out.println();
+		
+		
 	}
 
-	// METHOD 3- Create event with title, start time, end time
+	/*
+	 * METHOD 3- Create event with title, start time, end time
+	 */
 	public void createEvent(String eventTitle, DateTime startTime,
-			DateTime endTime) {
+			DateTime endTime) throws IOException, ServiceException {
 		CalendarEventEntry myEntry = new CalendarEventEntry();
-
 		myEntry.setTitle(new PlainTextConstruct(eventTitle));
 
 		When eventTimes = new When();
 		eventTimes.setStartTime(startTime);
 		eventTimes.setEndTime(endTime);
 		myEntry.addTime(eventTimes);
-
-		try {
-			myService.insert(eventFeedUrl, myEntry);
-		} catch (Exception e) {
-		}
+		myService.insert(eventFeedUrl, myEntry);
 	}
 
-	// METHOD 4- DELETE ALL EVENTS
-	public void deleteAllEvents() {
-		try {
-			CalendarEventFeed resultFeed;
-
-			resultFeed = myService.getFeed(eventFeedUrl,
-					CalendarEventFeed.class);
-			for (int i = 0; i < resultFeed.getEntries().size(); i++) {
-				CalendarEventEntry entry = resultFeed.getEntries().get(i);
-				entry.delete();
-			}
-		} catch (Exception e) {
-		}
-	}
-
-	// testing
-	public void insertBatchEvents(List<CalendarEventEntry> eventsToInsert) {
-		try {
-			CalendarEventFeed batchRequest = new CalendarEventFeed();
-			for (int i = 0; i < eventsToInsert.size(); i++) {
-				CalendarEventEntry toInsert = eventsToInsert.get(i);
-				BatchUtils.setBatchId(toInsert, String.valueOf(i));
-				BatchUtils.setBatchOperationType(toInsert,
-						BatchOperationType.INSERT);
-				batchRequest.getEntries().add(toInsert);
-			}
-
-			CalendarEventFeed feed;
-
-			feed = myService.getFeed(eventFeedUrl, CalendarEventFeed.class);
-
-			Link batchLink = feed.getLink(Link.Rel.FEED_BATCH, Link.Type.ATOM);
-			URL batchUrl = new URL(batchLink.getHref());
-
-		} catch (Exception e) {
-		}
-	}
-	
-	// testing
-	public CalendarEventEntry getNewEvent(String title, DateTime start,
-			DateTime end) {
-		CalendarEventEntry newEvent = new CalendarEventEntry();
-
-		newEvent.setTitle(new PlainTextConstruct(title));
+	/*
+	 * METHOD 3b- Overloaded. Create event with title, start time, end time
+	 */
+	public void createEvent(String eventTitle, String startTime, String endTime)
+			throws IOException, ServiceException {
+		CalendarEventEntry myEntry = new CalendarEventEntry();
+		myEntry.setTitle(new PlainTextConstruct(eventTitle));
 
 		When eventTimes = new When();
-		eventTimes.setStartTime(start);
-		eventTimes.setEndTime(end);
-		newEvent.addTime(eventTimes);
-
-		return newEvent;
+		eventTimes.setStartTime(DateTime.parseDateTime(startTime));
+		eventTimes.setEndTime(DateTime.parseDateTime(endTime));
+		myEntry.addTime(eventTimes);
+		myService.insert(eventFeedUrl, myEntry);
 	}
-	
+
 	/*
-	 * To Demonstrate how to use this class
-	 * 
-	 * 
-	   GoogleSync temp = new GoogleSync(userName, userPassword);
-
-		//temp.deleteAllEvents();
-
-		temp.createEvent("eventTitle0",
-				DateTime.parseDateTime("2012-03-01T22:40:00"),
-				DateTime.parseDateTime("2012-03-01T22:45:00"));
-				
-		temp.getAllEvents();
-		//temp.getgetRangeEvents(start,end);
-		
-
+	 * METHOD 4- DELETE ALL EVENTS
 	 */
+	public void deleteAllEvents() throws IOException, ServiceException {
+		CalendarEventFeed resultFeed;
+		resultFeed = myService.getFeed(eventFeedUrl, CalendarEventFeed.class);
+		for (int i = 0; i < resultFeed.getEntries().size(); i++) {
+			CalendarEventEntry entry = resultFeed.getEntries().get(i);
+			entry.delete();
+		}
+	}
 }
+
+/*
+ * // testing public void insertBatchEvents(List<CalendarEventEntry>
+ * eventsToInsert) { try { CalendarEventFeed batchRequest = new
+ * CalendarEventFeed(); for (int i = 0; i < eventsToInsert.size(); i++) {
+ * CalendarEventEntry toInsert = eventsToInsert.get(i);
+ * BatchUtils.setBatchId(toInsert, String.valueOf(i));
+ * BatchUtils.setBatchOperationType(toInsert, BatchOperationType.INSERT);
+ * batchRequest.getEntries().add(toInsert); }
+ * 
+ * CalendarEventFeed feed;
+ * 
+ * feed = myService.getFeed(eventFeedUrl, CalendarEventFeed.class);
+ * 
+ * Link batchLink = feed.getLink(Link.Rel.FEED_BATCH, Link.Type.ATOM); URL
+ * batchUrl = new URL(batchLink.getHref());
+ * 
+ * } catch (Exception e) { } }
+ * 
+ * // testing public CalendarEventEntry getNewEvent(String title, DateTime
+ * start, DateTime end) { CalendarEventEntry newEvent = new
+ * CalendarEventEntry();
+ * 
+ * newEvent.setTitle(new PlainTextConstruct(title));
+ * 
+ * When eventTimes = new When(); eventTimes.setStartTime(start);
+ * eventTimes.setEndTime(end); newEvent.addTime(eventTimes);
+ * 
+ * return newEvent; }
+ */
