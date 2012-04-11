a051acac7e45976b61566370909b50b9ea3b0ae3
mkwai2012 <mkwai2012@dbd0592c-cb16-c347-c70a-97c1e5d1de72>
2012-04-11 16:49:08 +0000

    tempary use -> get data from DB

--- AndroidCalendar2/src/com/calendar/TodoList.java
+++ AndroidCalendar2/src/com/calendar/TodoList.java
@@ -2,17 +2,23 @@ package com.calendar;
 
 import java.util.Calendar;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import com.test2.R;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class TodoList extends Activity {
@@ -20,7 +26,9 @@ public class TodoList extends Activity {
 	protected Calendar currentDateCalendar = Calendar.getInstance();
 	Button addTask, sorting;
 	TextView today;
-	
+	JSONArray titleT;
+	private LinearLayout listLayout;
+	//ListView list = (ListView) findViewById(R.id.todoList);  
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -51,5 +59,44 @@ public class TodoList extends Activity {
 			}
 			
 		});
+		
+		sorting.setOnClickListener(new OnClickListener(){
+			
+			@Override
+			public void onClick(View arg1) {
+				
+				Log.i("sort", "test");
+				Bundle extras = getIntent().getExtras();
+				
+//		        try {
+		        	titleT = AndroidCalendar2Activity.getDB().fetchConditional("TaskTable","");
+//				} catch (JSONException e) {Log.i("json",e.toString());}
+				
+				if (titleT.length()>0) {
+					Log.i("sort", ">0");
+					try{
+						for(int i = 0;i<titleT.length();i++)
+							Log.i(i+"", titleT.getJSONObject(i).getString("title"));
+						//To-do...
+					}catch(Exception e){
+			        	Log.i("h",e.toString());
+			        }
+/*			        try{
+			        	for(int i = 0;i<titleT.length();i++){
+			        		TextView TV = new TextView(this);
+			        		TV.setText(titleT.getJSONObject(i).getString("name"));
+			        		listLayout.addView(TV,i);
+			        	}
+			        	
+			        }catch(Exception e){
+			        	Log.i("h",e.toString());
+			        }
+*/				
+				}
+				else Log.i("sort", "else");
+			}
+
+		});
+			
 	}
 }
