1638f622df06fb4c85331ff027d75a5e3dc073be
klhoab <klhoab@gmail.com>
2012-04-12 07:25:53 +0000

    photolink and googlesync

---
+++ AndroidCalendar2/src/com/photolink/ViewImage.java
@@ -0,0 +1,27 @@
+// By selecting the thumbnails user can view the actual image.
+package com.photolink;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public class ViewImage extends Activity {
+      private String filename;
+      @Override
+      public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            System.gc();
+            Intent i = getIntent();
+            Bundle extras = i.getExtras();
+            BitmapFactory.Options bfo = new BitmapFactory.Options();
+            bfo.inSampleSize = 2;
+            filename = extras.getString("filename");
+            ImageView iv = new ImageView(getApplicationContext());
+            Bitmap bm = BitmapFactory.decodeFile(filename, bfo);
+            iv.setImageBitmap(bm);
+            setContentView(iv);
+      } 
+}
