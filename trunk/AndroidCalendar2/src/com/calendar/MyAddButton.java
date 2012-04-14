18195000adfdaeb809f8f55ac0cef8b00acc50d0
chanharry11 <chanharry11@dbd0592c-cb16-c347-c70a-97c1e5d1de72>
2012-04-14 17:43:02 +0000

    monthly view interface modified & part of the sharing system

---
+++ trunk/AndroidCalendar2/src/com/calendar/MyAddButton.java
@@ -0,0 +1,13 @@
+package com.calendar;
+
+import android.content.Context;
+import android.view.View;
+
+public class MyAddButton extends View{
+
+	public MyAddButton(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
