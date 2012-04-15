e8d61a94f43b160ff7bf64d91bc2b903711b2ef0
mkwai2012 <mkwai2012@dbd0592c-cb16-c347-c70a-97c1e5d1de72>
2012-04-15 17:05:16 +0000

    skeleton of location alarm

---
+++ trunk/AndroidCalendar2/src/com/Alarm/LocationAlarmService.java
@@ -0,0 +1,15 @@
+package com.Alarm;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class LocationAlarmService extends Service{
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
